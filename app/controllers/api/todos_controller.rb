ecf9ae84a53fd5226ef2291c70cba1ce399c2165
ykttdn <38461803+ykttdn@users.noreply.github.com>
2024-10-25 01:46:41 +0900

    Add show action

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -2,10 +2,20 @@
 
 module Api
   class TodosController < ApplicationController
+    RECORD_NOT_FOUND = 'Record was not found'
+
     def index
       todos = Todo.all
 
       render json: todos
+    end
+
+    def show
+      todo = Todo.find(params[:id])
+
+      render json: { todo:, error: nil }
+    rescue ActiveRecord::RecordNotFound
+      render json: { todo: nil, error: [RECORD_NOT_FOUND] }, status: :not_found
     end
   end
 end
