9a3a79907875b344ac9c5e65f1fb600ef5ed7f63
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-01-07 00:11:48 +0900

    Enable fetching todos from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  type Todo = {
+    id: number,
+    title: string,
+    created_at: string,
+    updated_at: string
+  }
+
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [error, setError] = useState<unknown>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true)
+
+      try {
+        const response = await fetch('http://localhost:3000/api/todos')
+
+        if (response.ok) {
+          const todos: Todo[] = await response.json()
+          setTodos(todos)
+        } else {
+          setError('Unhandled error')
+        }
+      } catch (error) {
+        console.error(error)
+        setError(error)
+      }
+
+      setIsLoading(false)
+    }
+
+    fetchTodos()
+  }, [])
 
   return (
     <>
@@ -28,6 +63,18 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {isLoading ? (
+        <p>Fetching todos...</p>
+      ) : error ? (
+        <p>Error occurred while fetching todos</p>
+      ):(
+        todos && <ol>
+          {todos.map((todo) => {
+            return <li key={todo.id}>{todo.title}</li>
+          })}
+        </ol>
+      )}
     </>
   )
 }
