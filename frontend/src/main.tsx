a351a56c013fa49faf735aef318c9cbda3adcc13
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-01-12 12:14:51 +0900

    Install React Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.tsx';
 
 // biome-ignore lint/style/noNonNullAssertion:
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
