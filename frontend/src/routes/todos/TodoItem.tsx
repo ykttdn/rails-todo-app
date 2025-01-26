83fa5a9c57b20368ba181dc45ca21dba4b5f223d
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-01-27 01:41:54 +0900

    Add form

--- frontend/src/routes/todos/TodoItem.tsx
+++ frontend/src/routes/todos/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button';
+import { type SubmitHandler, useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 import { useGetRequest } from '../../hooks/useGetRequest';
 import type { Todo } from './types/todo';
@@ -7,6 +9,8 @@ type Response = {
   error: [];
 };
 
+type EditablePartOfTodo = Omit<Todo, 'id' | 'created_at' | 'updated_at'>;
+
 export function TodoItem() {
   const { id } = useParams();
 
@@ -14,6 +18,16 @@ export function TodoItem() {
     `http://localhost:3000/api/todos/${id}`,
   );
   const todo = data?.todo;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty },
+  } = useForm<EditablePartOfTodo>();
+
+  const onSubmit: SubmitHandler<EditablePartOfTodo> = (formData) => {
+    console.log(formData);
+  };
 
   if (isLoading) {
     return <p>Fetching todo...</p>;
@@ -26,12 +40,19 @@ export function TodoItem() {
   return (
     <>
       {todo && (
-        <ul>
-          <li>{todo.id}</li>
-          <li>{todo.title}</li>
-          <li>{todo.created_at}</li>
-          <li>{todo.updated_at}</li>
-        </ul>
+        <>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="title">Title</label>
+            <input id="title" defaultValue={todo.title} {...register('title')} />
+            <Button type="submit" disabled={!isDirty}>
+              Update
+            </Button>
+          </form>
+          <ul>
+            <li>created: {todo.created_at}</li>
+            <li>updated: {todo.updated_at}</li>
+          </ul>
+        </>
       )}
     </>
   );
