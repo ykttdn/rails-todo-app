f792b0f9d3acece7d9c60e1053273ba279588731
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-02-10 01:12:31 +0900

    feat: Add delete button

--- frontend/src/routes/todos/Index.tsx
+++ frontend/src/routes/todos/Index.tsx
@@ -26,13 +26,18 @@ export function Index() {
           <ol className="flex flex-col gap-2">
             {todos.map((todo) => {
               return (
-                <li key={todo.id} className="rounded-lg border-2 border-gray-200">
+                <li key={todo.id} className="flex items-center rounded-lg border-2 border-gray-200">
                   <Link
                     to={`todos/${todo.id}`}
-                    className="block p-6 text-2xl font-semibold text-gray-600 hover:text-orange-500"
+                    className="block grow p-6 text-2xl font-semibold text-gray-600 hover:text-orange-500"
                   >
                     {todo.title}
                   </Link>
+                  <div className="pr-4">
+                    <button type="button" className="btn btn-outline btn-error">
+                      Delete
+                    </button>
+                  </div>
                 </li>
               );
             })}
