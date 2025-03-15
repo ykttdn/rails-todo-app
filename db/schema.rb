48b4c34de5aa6b8701e1c6a7aaa300a04dba634c
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-03-16 00:16:10 +0900

    feat: Create table habits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_25_160828) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_15_150714) do
+  create_table "habits", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
