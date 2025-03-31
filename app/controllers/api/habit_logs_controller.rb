5717792ceddc488d444ee5fecb0b7231defd48fe
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-03-31 23:04:56 +0900

    feat: Add route for fetching all logs

--- app/controllers/api/habit_logs_controller.rb
+++ app/controllers/api/habit_logs_controller.rb
@@ -7,5 +7,32 @@ module Api
 
       render json: habit_logs
     end
+
+    def index_all
+      habit_logs = HabitLog.where(executed_at: from..to)
+                           .order(:executed_at)
+                           .map do |habit_log|
+                             {
+                               date: habit_log.executed_at,
+                               level: habit_log.level
+                             }
+                           end
+
+      render json: habit_logs
+    end
+
+    private
+
+    def from
+      Date.parse(params[:from].to_s)
+    rescue Date::Error # also when params[:from] is nil
+      Date.current.beginning_of_month
+    end
+
+    def to
+      Date.parse(params[:to].to_s)
+    rescue Date::Error # also when params[:to] is nil
+      Date.current.end_of_month
+    end
   end
 end
--- spec/requests/api/habit_logs_spec.rb
+++ spec/requests/api/habit_logs_spec.rb
@@ -19,4 +19,12 @@ RSpec.describe 'Api::HabitLogs' do
       expect(response.parsed_body.length).to eq(logs_count)
     end
   end
+
+  describe 'GET /api/habit_logs' do
+    it 'returns a 200 OK status' do
+      get '/api/habit_logs', params: { from: '2025-01-01', to: '2025-01-31' }
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
